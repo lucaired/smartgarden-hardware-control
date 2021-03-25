0662bd4191d95d3d86baf720e18dcda65fe65ce4
lucaired <aired@posteo.hu>
2021-03-25 22:33:49 +0100

    add rusty rocket

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,34 @@
 
 #[macro_use] extern crate rocket;
 
+/// This is an inclusive interval
+const LOWEST_ALLOWED_PORT: i32 = 2;
+const HIGHEST_ALLOWED_PORT: i32 = 5;
+
 #[get("/fan/<number>/on")]
-fn hello(number: u8) -> String {
-    format!("Hello, fan {} turned on!", number)
+fn fan_on(number: i32) -> String {
+    if port_number_ok(number) {
+        format!("Hello, fan {} turned on!", number)
+    } else {
+        format!("Hello, fan {} could not be turned on!", number)
+    }
 }
 
-#[get("/fan/<number>/off")]
-fn hello(number: u8) -> String {
-    format!("Hello, fan {} turned off!", number)
+#[get("/off/<number>")]
+fn fan_off(number: i32) -> String {
+    if port_number_ok(number) {
+        format!("Hello, fan {} turned off!", number)
+
+    } else {
+        format!("Hello, fan {} could not be turned off!", number)
+    }
+}
+
+// TODO: make a result
+fn port_number_ok(port_number: i32) -> bool {
+    LOWEST_ALLOWED_PORT <= port_number && port_number <= HIGHEST_ALLOWED_PORT
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite().mount("/", routes![fan_on]).launch();
 }
