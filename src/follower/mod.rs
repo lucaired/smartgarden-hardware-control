d01debd68a922cbcb53b4c2fd64f300da41464e1
lucaired <aired@posteo.hu>
2021-04-01 08:40:54 +0200

    fmt

--- src/follower/mod.rs
+++ src/follower/mod.rs
@@ -1,6 +1,6 @@
 use isahc::prelude::*;
 
-pub async fn set_fan(fan_ressource: &str, state: &str) -> Result<(), isahc::Error>{
+pub async fn set_fan(fan_ressource: &str, state: &str) -> Result<(), isahc::Error> {
     let mut response = isahc::get(&format!("{}/{}", fan_ressource, state))?;
     print!("{}", response.text()?);
     Ok(())
