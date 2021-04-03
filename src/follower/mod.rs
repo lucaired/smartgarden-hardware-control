fa33cacf363f79d0a09003539b5d6e0323228faa
lucaired <aired@posteo.hu>
2021-04-03 12:01:34 +0200

    Use get and not head request

--- src/follower/mod.rs
+++ src/follower/mod.rs
@@ -5,6 +5,6 @@ pub fn set_state_to_fan(state: &str) -> Result<(), Box<dyn std::error::Error>> {
     let request_url = format!("http://localhost:8100/fan/{}", state);
 
     let client = ClientBuilder::new().build()?;
-    let _response = client.head(&request_url).send()?;
+    let _response = client.get(&request_url).send()?;
     Ok(())
 }
